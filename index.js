8a67cc4a0aa58b255b6ac7abe938edcee95403d2
May Carandang <maytiara@gmail.com>
2022-07-17 23:48:41 +0800

    Add all files incl. starter code

---
+++ index.js
@@ -0,0 +1,6 @@
+// Install the co-dependencies [npm i inquirer@8.2.4]
+// Objective: To create a README.md file for your github proj
+
+// Create a demo file or mock-up README.md file for your template
+// Create Questions, based from the module 9 requirements
+

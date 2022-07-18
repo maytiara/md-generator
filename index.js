8e93dcda940e3a4a60ef4a83a97a3c3b15fe1393
May Carandang <maytiara@gmail.com>
2022-07-18 13:56:41 +0800

    Syntax prompt question for console.log (ans)

--- index.js
+++ index.js
@@ -1,8 +1,54 @@
 // Install the co-dependencies [npm i inquirer@8.2.4]
 // Objective: To create a README.md file for your github proj
 
+const inquirer = require ('inquirer');
+
+
 // Create a demo file or mock-up README.md file for your template
 // Create Questions, based from the module 9 requirements
+inquirer.prompt([ 
+    {
+        type: 'input',
+        message: "What is the Project title?",
+        name: 'title',
+    },
+    {
+        type: 'list',
+        message: "Please choose the license type",
+        name: 'license',
+        choices: [
+            'CC BY 4.0', 'APACHE 2.0', 'MIT', 'MS PL', 'ISC', 'GPL 3.0', 'OSL 3.0', 'AGPL 3.0', 'LGPL 3.0', 'UNLICENSE', 
+        ]
+    },
+    {
+        type: 'input',
+        message: "What are the steps required for this project?",
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: "Provide link of your demo file",
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: "Provide the list of your reference",
+        name: 'reference',
+    },
+    {
+        type: 'input',
+        message: 'What is your github username',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: "Please provide your email address",
+        name: 'email',
+    },
+    
+]).then ((ans) => {
+    console.log(ans);
+})
 
 // Project Title
 // License

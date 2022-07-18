de667a923586829fd752c6214a9eb602f960590f
May Carandang <maytiara@gmail.com>
2022-07-18 13:32:45 +0800

    Create pseudo code

--- index.js
+++ index.js
@@ -4,3 +4,15 @@
 // Create a demo file or mock-up README.md file for your template
 // Create Questions, based from the module 9 requirements
 
+// Project Title
+// License
+// Description 
+// Table of Contents - Default
+// Installation
+// Usage
+// Reference
+// Github Link
+// Feedback or contribute
+// License
+
+// After asked questions, to generate a README.md file to output folder

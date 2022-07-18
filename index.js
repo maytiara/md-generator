ecf1f0d425b3892a804f51665e496524b1824218
May Carandang <maytiara@gmail.com>
2022-07-18 14:56:04 +0800

    added variable for function generateMarkdown

--- index.js
+++ index.js
@@ -2,63 +2,67 @@
 // Objective: To create a README.md file for your github proj
 
 const inquirer = require ('inquirer');
+const generateMarkdown = require('./utils/md-generator');
 
 
 // Create a demo file or mock-up README.md file for your template
 // Create Questions, based from the module 9 requirements
+
+// Table of Contents - Default ---
+
 inquirer.prompt([ 
     {
         type: 'input',
         message: "What is the Project title?",
-        name: 'title',
+        name: 'title', // Project Title
     },
     {
         type: 'list',
         message: "Please choose the license type",
-        name: 'license',
+        name: 'license', // License
         choices: [
             'CC BY 4.0', 'APACHE 2.0', 'MIT', 'MS PL', 'ISC', 'GPL 3.0', 'OSL 3.0', 'AGPL 3.0', 'LGPL 3.0', 'UNLICENSE', 
         ]
     },
     {
         type: 'input',
+        message: "Describe the ojective of this project",
+        name: 'description', // Description 
+    },
+    {
+        type: 'input',
         message: "What are the steps required for this project?",
-        name: 'installation',
+        name: 'installation', // Installation
     },
     {
         type: 'input',
         message: "Provide link of your demo file",
-        name: 'usage',
+        name: 'usage', // Usage
     },
     {
         type: 'input',
-        message: "Provide the list of your reference",
-        name: 'reference',
+        message: "Provide the link of your reference",
+        name: 'reference', // Reference
     },
     {
         type: 'input',
         message: 'What is your github username',
-        name: 'github',
+        name: 'github', // Github Link
     },
     {
         type: 'input',
         message: "Please provide your email address",
-        name: 'email',
+        name: 'email', // Feedback or contribute
     },
-    
+
 ]).then ((ans) => {
-    console.log(ans);
+    console.log(ans); // asked in the node environment
+
+    const generated = generateMarkdown (ans); // return list of data inserted in function generateMarkdown
+
+    console.log(generated);
 })
 
-// Project Title
-// License
-// Description 
-// Table of Contents - Default
-// Installation
-// Usage
-// Reference
-// Github Link
-// Feedback or contribute
-// License
+
 
 // After asked questions, to generate a README.md file to output folder

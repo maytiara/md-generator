3a7a962ce8eb14e782c587e24c8013591f0fe612
May Carandang <maytiara@gmail.com>
2022-07-18 14:16:52 +0800

    create function to generateMd & added list of data

---
+++ utils/md-generator.js
@@ -0,0 +1,22 @@
+
+//list of data required for the generator
+function generateMarkdown ({ 
+    title,
+    license,
+    installation,
+    usage,
+    reference,
+    github,
+    email
+}) {
+
+    // print the data/s on this string (template literal) **Dont use tab indentation inside the string
+   return `
+
+
+
+` 
+
+}
+
+module.exports = generateMarkdown; // this export the function generateMarkdown
